b72f31a76a237b7dadc8c7eb51851339f665fc93
Edoardo Ferocino <edoardo.ferocino@polimi.it>
2020-10-28 18:13:27 +0100

    Speed up of "DatRead" Custom improvements of save_figure

--- MatlabTools/save_figure.m
+++ MatlabTools/save_figure.m
@@ -16,6 +16,15 @@ end
 
 for ih = 1:NumFig
     set(fig_handle(ih), 'Color', 'w');
+    graphics = findobj(fig_handle(ih),'type','uipanel');
+    if ~isempty(graphics)
+        grcols = zeros(numel(graphics),3);
+        for ig = 1:numel(graphics)
+           grcols(ig,:) = graphics(ig).BackgroundColor;
+           graphics(ig).BackgroundColor = 'w'; 
+        end
+    end
+    set(fig_handle(ih), 'Color', 'w');
 %     set(fig_handle(ih), 'PaperPosition', [-0.5 -0.25 6 5.5]); %Position the plot further to the left and down. Extend the plot to fill entire paper.
     set(fig_handle(ih), 'PaperSize', [5 5]); %Keep the same paper size
     set(fig_handle(ih),'PaperPositionMode','auto');
@@ -35,6 +44,12 @@ for ih = 1:NumFig
         CustomVarargin(contains(CustomVarargin,'-svg')) = [];
     else
         export_fig(Nome,'-painters','-png',CustomVarargin{:},fig_handle(ih));
+    end
+    set(fig_handle(ih),'Color','default');
+    if ~isempty(graphics)
+        for ig = 1:numel(graphics)
+           graphics(ig).BackgroundColor = grcols(ig,:);
+        end
     end
 end
 if nargout
